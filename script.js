f01b04eafe95a5f720156483361a5ebd833df763
eriksson <sebbe.eriksson09@icloud.com>
2019-08-19 15:24:45 +0200

    yo mamma

---
+++ script.js
@@ -0,0 +1,18 @@
+anime({
+  targets: ".test",
+  translateY: 1500,
+  duration: 3500,
+  loop: true,
+  easing: "easeInOutSine",
+  direction: "alternate",
+  backgroundColor: ["#ffff00", "#ff471a"]
+});
+
+anime({
+  targets: "body",
+  duration: 3500,
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutSine",
+  backgroundColor: ["#b3ecff", "#0086b3"]
+});
